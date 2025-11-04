a880b26f06299f924f24d7ad5b812cbd701c3eaa
Diego Alckmin <alckmindiego@gmail.com>
2025-11-04 16:04:34 -0300

    fix: Corrige script de build para exportar FLASK_APP.

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Instala as dependências
 pip install -r requirements.txt
 
+# Exporta a variável de ambiente para que o Flask encontre a aplicação
+export FLASK_APP=app.py
+
 # Cria as tabelas do banco de dados
 flask db init
 flask db migrate -m "Initial migration."
